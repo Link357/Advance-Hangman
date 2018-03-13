ef162096ff29d1cc283546fc18a9dbf8fc983536
David Turner <Dturner753@yahoo.com>
2018-03-12 20:02:46 -0700

    finished letter and start of word js

---
+++ letter.js
@@ -0,0 +1,9 @@
+var Word = require("./word.js");
+for(i = 0; i < str.length; i++) {
+}
+
+var Letter = function(){
+    
+}
+
+module.exports = Letter;
---
+++ word.js
@@ -0,0 +1,5 @@
+var Letter = require("./letter.js");
+for (i = 0; i < array.length; i++)
+
+
+module.exports = Word;
