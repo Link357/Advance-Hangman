bec09a583e893beab6ffe5e416cc9afebbe11343
David Turner <Dturner753@yahoo.com>
2018-03-12 21:16:02 -0700

    advance hangman turn in

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 var Letter = require("./letter.js");
 var Word = require("./word.js");
-
+for (i=0; i < Word.wordChoice.length; i++){
+    function start() {
+        console.log(Letter.underScore[i])
+        inquirer
+          .prompt({
+            name: "guess",
+            type: "input",
+            message: "guess a letter",
+          })
+          .then(function(answer) {
+            // based on their answer, either call the bid or the post functions
+            var userInput = answer.guess 
+            Letter.letter = GuessuserInput
+            Letter.guess();
+            Letter.result();
+          });
+      }
+}
